49814ebf980334f56d70c64036140988f1d32c39
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-09-26 13:48:03 +0700

    prak 4, langkah 4

---
+++ week-05/src/hello_world/lib/basic_widgets/loading_cupertino.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadingCupertino extends StatelessWidget {
+  const LoadingCupertino({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        margin: const EdgeInsets.only(top: 30),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            CupertinoButton(
+              child: const Text("Contoh button"),
+              onPressed: () {},
+            ),
+            const CupertinoActivityIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ week-05/src/hello_world/lib/basic_widgets/text_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyTextWidget extends StatelessWidget {
+  const MyTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      "Nama saya Ziedny, sedang belajar Pemrograman Mobile",
+      style: TextStyle(
+        color: Colors.red,
+        fontSize: 14,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
