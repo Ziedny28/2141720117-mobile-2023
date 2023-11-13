8d568f4efeeaad5ee0febabf581ce27d1049e04a
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-11-13 22:59:12 +0700

    W12: Soal 13

--- week-12/src/books/lib/view/geolocation.dart
+++ week-12/src/books/lib/view/geolocation.dart
@@ -10,33 +10,39 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   String myPosition = '';
+  Future<Position>? position;
 
   @override
   void initState() {
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition =
-          'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
   }
 
   @override
   Widget build(BuildContext context) {
-    final myWidget =
-        myPosition == '' ? const CircularProgressIndicator() : Text(myPosition);
     return Scaffold(
       appBar: AppBar(title: const Text('Current Location Ziedny')),
-      body: Center(child: myWidget),
+      body: Center(
+          child: FutureBuilder(
+        future: position,
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            return Text(snapshot.data.toString());
+          } else {
+            return const Text('');
+          }
+        },
+      )),
     );
   }
 
   Future<Position> getPosition() async {
     await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
-    Position? position = await Geolocator.getCurrentPosition();
+    await Future.delayed(const Duration(seconds: 3));
+    Position position = await Geolocator.getCurrentPosition();
     return position;
   }
 }
