8bbb4ceb8af311d3aae7b885ab40782d31470c4f
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-11-13 10:30:09 +0700

    week 12 initial commit, prak1

---
+++ week-12/src/books/lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo Ziedny',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the feature Ziedny'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                getData().then((value) {
+                  setState(() {
+                    result = value.body.toString().substring(0, 450);
+                  });
+                }).catchError((_) {
+                  setState(() {
+                    result = 'An Error Occured';
+                  });
+                });
+              },
+              child: const Text('Go!'),
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/94Y8PgAACAAJ';
+    Uri uri = Uri.https(authority, path);
+    return http.get(uri);
+  }
+}
