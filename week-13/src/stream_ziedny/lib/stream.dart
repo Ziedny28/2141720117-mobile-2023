6817a3894050381d7ca5e6a45216aff710ec09c6
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-11-20 09:12:31 +0700

    W13: Jawaban Soal 2

---
+++ week-13/src/stream_ziedny/lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+  ];
+}
